0f8fa48392ac15488d46f5e6d236fa22b64408d3
clinton wambugu <clintonwambugu@gmail.com>
2020-07-27 16:22:06 +0300

    Add a few gems

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def article_params
-    params.require(:article).permit(:title, :body, :taglist)
+    params.require(:article).permit(:title, :body, :taglist, :image)
   end
 end
