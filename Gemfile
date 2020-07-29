73e6d50904d1051cc1ea0cb5005f159bfd8b9655
clinton wambugu <clintonwambugu@gmail.com>
2020-07-29 15:00:01 +0300

    change to psg db create pg db

--- Gemfile
+++ Gemfile
@@ -5,8 +5,14 @@ ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
+group :development, :test do 
+   gem 'sqlite3'    #gem to use in development-test environment
+end
+
+group :production do 
+  gem 'pg'         #gem to use in production environment
+end
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
---
+++ app/controllers/author_sessions_controller.rb
@@ -0,0 +1,20 @@
+class AuthorSessionsController < ApplicationController
+  before_action :require_login, only: [:new, :create, :destroy]
+
+  def new
+  end
+
+  def create
+    if login(params[:email], params[:password])
+      redirect_back_or_to(articles_path, notice: 'Logged in successfully.')
+    else
+      flash.now.alert = "Login failed."
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:authors, notice: 'Logged out!')
+  end
+end
